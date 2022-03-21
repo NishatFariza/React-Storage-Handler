509724a51269a8eb33c86eb1781bef85d08c657f
NishatFariza <nishatfarzia78@gmail.com>
2022-03-22 03:00:27 +0600

    import default

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Cosmetic from "./components/Cosmetics/Cosmetic";
+import Shoes from "./components/Shoes/Shoes";
 
 function App() {
   return (
     <div className="App">
       <Cosmetic></Cosmetic>
+      <Shoes></Shoes>
     </div>
   );
 }
--- src/components/Shoes/Shoes.jsx
+++ src/components/Shoes/Shoes.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { add, multiply } from '../../utilites/calculate';
 
 const Shoes = () => {
     const first = 13;
     const second = 25;
+    const result = multiply(first, second);
+    const sum = add(first, second);
     return (
         <div>
             <h1>This is Shoes Component</h1>
+            <p>Result: {result} and Total: {sum}</p>
         </div>
     );
 };
