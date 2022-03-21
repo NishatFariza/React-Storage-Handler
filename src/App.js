4ab5a7d9886d8b7481f9abab84093bce8a553464
NishatFariza <nishatfarzia78@gmail.com>
2022-03-22 02:54:58 +0600

    export component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Cosmetic from "./components/Cosmetics/Cosmetic";
 
 function App() {
   return (
     <div className="App">
+      <Cosmetic></Cosmetic>
     </div>
   );
 }
---
+++ src/components/Shoes/Shoes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Shoes = () => {
+    const first = 13;
+    const second = 25;
+    return (
+        <div>
+            <h1>This is Shoes Component</h1>
+        </div>
+    );
+};
+
+export default Shoes;
