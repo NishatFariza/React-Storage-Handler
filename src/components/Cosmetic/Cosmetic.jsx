e8e36f1c28a72765c5eb49c9353abaffe8bd2bc7
NishatFariza <nishatfarzia78@gmail.com>
2022-03-23 03:00:34 +0600

    json genaretor data

--- src/components/Cosmetic/Cosmetic.jsx
+++ src/components/Cosmetic/Cosmetic.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Cosmetic = (props) => {
-    console.log(props);
+     const {name, id, price} =props.data;
+     
+//     console.log(props);
         return (
         <div className='cosmetic-container'>
-             <h1>Name: {props.data.name}</h1>
-             <h1>Id: {props.data.id}</h1>
-             <h1>Price: {props.data.price}</h1>
+             <h1>Name: {name}</h1>
+             <h1>Id: {id}</h1>
+             <h1>Price: {price}</h1>
         </div>
      )
 }
--- src/components/Cosmetics/Cosmetics.jsx
+++ src/components/Cosmetics/Cosmetics.jsx
@@ -4,16 +4,59 @@ import './Cosmetics.css'
 
 const Cosmetics = () => {
     const cosmetics =[
-        {id:1, name: 'Alta', price: 100},
-        {id:2, name: 'Churi', price: 600},
-        {id:3, name: 'Nopor', price: 400},
-        {id:4, name: 'Palish', price: 700},
-        {id:5, name: 'Nalish', price: 900},
-    ]
+        {
+          "id": "623a387602815ddc6c01494d",
+          "index": 0,
+          "balance": "$2,708.52",
+          "picture": "http://placehold.it/32x32",
+          "age": 28,
+          "name": "Melisa Kirkland",
+          "gender": "female"
+        },
+        {
+          "id": "623a38761656260e79151a2b",
+          "index": 1,
+          "balance": "$3,964.80",
+          "picture": "http://placehold.it/32x32",
+          "age": 38,
+          "name": "Tonya Hoffman",
+          "gender": "female"
+        },
+        {
+          "id": "623a3876b2c1907202319721",
+          "index": 2,
+          "balance": "$1,630.02",
+          "picture": "http://placehold.it/32x32",
+          "age": 30,
+          "name": "Cooley Craft",
+          "gender": "male"
+        },
+        {
+          "id": "623a38767047641a5d9bf4fb",
+          "index": 3,
+          "balance": "$1,623.25",
+          "picture": "http://placehold.it/32x32",
+          "age": 23,
+          "name": "Morin Franks",
+          "gender": "male"
+        },
+        {
+          "id": "623a3876d272a1bd78350491",
+          "index": 4,
+          "balance": "$2,259.13",
+          "picture": "http://placehold.it/32x32",
+          "age": 25,
+          "name": "Rhea Kirby",
+          "gender": "female"
+        }
+      ]
     return (
         <div>
             {
-                cosmetics.map(cosmetic =><Cosmetic data={cosmetic}></Cosmetic>)
+                cosmetics.map(cosmetic =><Cosmetic
+                     data={cosmetic}
+                     key={cosmetic.id}>
+                     </Cosmetic>)
                     
             }
         </div>
