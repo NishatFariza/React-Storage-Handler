0772c3000548203a4563c295f3a487f2d5508102
NishatFariza <nishatfarzia78@gmail.com>
2022-03-23 03:49:12 +0600

    add event  handler

--- src/components/Cosmetic/Cosmetic.jsx
+++ src/components/Cosmetic/Cosmetic.jsx
@@ -3,14 +3,17 @@ import './Cosmetic.css'
 
 const Cosmetic = (props) => {
      const {name, id, price} =props.data;
-     
 //     console.log(props);
+        const addToCart = (id) =>{
+             console.log('item added', id);
+        }
+        const addToCartWithParam = () => addToCart(id);
         return (
         <div className='product'>
              <h1>Name: {name}</h1>
              <h1>Id: {id}</h1>
              <h1>Price: {price}</h1>
-             <button>Add To Cart</button>
+             <button onClick={addToCartWithParam}>Add To Cart</button>
         </div>
      )
 }
