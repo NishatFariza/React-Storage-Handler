69a750c88466d10788fddb23cca6819fb878db39
NishatFariza <nishatfarzia78@gmail.com>
2022-03-23 08:37:51 +0600

    add item in local storage

--- src/utilites/fakedb.jsx
+++ src/utilites/fakedb.jsx
@@ -1,6 +1,16 @@
 //use local storage to manage cart data
 const addToDb = id => {
-    localStorage.setItem(id, 1)
+   const quality = localStorage.getItem(id);
+
+   if(quality){
+       console.log('Already exists');
+       const newQuantity = quality + 1;
+       localStorage.setItem(id, newQuantity)
+   }
+   else{
+       console.log('New Item');
+         localStorage.setItem(id, 1);
+   }
 }
 
 export {addToDb}
