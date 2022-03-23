74ee284ee9ded345e09304287783355bdecfa647
NishatFariza <nishatfarzia78@gmail.com>
2022-03-23 09:27:45 +0600

    multiple data local storege

--- src/utilites/fakedb.jsx
+++ src/utilites/fakedb.jsx
@@ -1,16 +1,34 @@
 //use local storage to manage cart data
 const addToDb = id => {
-   const quality = localStorage.getItem(id);
+    let shoppingCart = {};
+    // localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 
+
+    //getting the shopping cart
+    const storedCart = localStorage.getItem('shopping-cart');
+
+    if (storedCart){
+        shoppingCart =JSON.parse(storedCart);
+    }
+    else{
+        shoppingCart = {};
+    }
+
+     //add quantity
+   const quality = shoppingCart[id];
    if(quality){
-       console.log('Already exists');
+    //    console.log('Already exists');
        const newQuantity = quality + 1;
-       localStorage.setItem(id, newQuantity)
+       shoppingCart[id] = newQuantity;
+    //    localStorage.setItem(id, newQuantity)
    }
    else{
-       console.log('New Item');
-         localStorage.setItem(id, 1);
+       shoppingCart[id] = 1;
+    //    console.log('New Item');
+    //      localStorage.setItem(id, 1);
    }
+   localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
+
 }
 
 export {addToDb}
